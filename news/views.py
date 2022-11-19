54eeffcec71ce8f7c4c882fe93290da95f240fda
Dmitriy <happy_river-horse@mail.ru>
2022-11-19 18:59:32 +0400

    first commit

---
+++ news/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from .models import News, Category
+from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, 'news/index.html', context=context)
+
+def test(request):
+    return HttpResponse('<h1>Работаю над этим</h1>')
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
